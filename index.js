99c895157b8d8aec50f9589f9c731c4e10c47f4c
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-16 18:51:52 -0400

    pls work 🥺

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-const fetchData = async (searchTerm) =>{
-    const response = await axios.get("http://www.omdbapi.com/",{
-        params: {
-            apikey: '65d9cda5', //1000 a day limit and free don't need to hide 
-            s: searchTerm
-        }
-    });
-    if(response.data.Error){
-        return [];
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '65d9cda5',
+      s: searchTerm
     }
-    return response.data.Search;
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
-   
-        const movies = await fetchData(event.target.value);
-        for (let movie of movies){
-            const div = document.createElement('div');
+  const movies = await fetchData(event.target.value);
 
-            div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
-            
-            `;
-            document.querySelector('#target').appendChild('div');
-        }
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${movie.Poster}" />
+      ${movie.Title}
+    `;
+
+    resultsWrapper.appendChild(option);
+  }
 };
-
 input.addEventListener('input', debounce(onInput, 500));
