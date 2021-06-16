472eb2974c478fbe353646cad885f03c3fcf2f18
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-16 16:47:44 -0400

    added debouncing input

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const fetchData = async () =>{
+const fetchData = async (searchTerm) =>{
     const response = await axios.get("http://www.omdbapi.com/",{
         params: {
-            apikey: '65d9cda5',
-            s: ''
+            apikey: '65d9cda5', //1000 a day limit and free don't need to hide 
+            s: searchTerm
         }
     });
+    console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = event => {
+    if(timeoutId){
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() =>{
+        fetchData(event.target.value);
+    }, 500)
+    
+};
+
+input.addEventListener('input', onInput);
