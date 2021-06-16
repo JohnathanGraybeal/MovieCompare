457457f342c2a960d593986385cc525ef7ea6a1e
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-16 19:12:03 -0400

    increased search speed and added logic to remove dropdown when user clicks outside of it

--- index.js
+++ index.js
@@ -30,18 +30,25 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-
+  resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
-
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     option.classList.add('dropdown-item');
     option.innerHTML = `
-      <img src="${movie.Poster}" />
+      <img src="${imgSrc}" />
       ${movie.Title}
     `;
 
     resultsWrapper.appendChild(option);
   }
 };
-input.addEventListener('input', debounce(onInput, 500));
+input.addEventListener('input', debounce(onInput, 200));
+
+document.addEventListener('click',event =>{
+  if(!root.contains(event.target)){
+
+    dropdown.classList.toggle('is-active');
+  }
+})
