ccca8406248f92be113c039eda52b5ea9dd01645
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-16 15:29:24 -0400

    created api get request using axios didn't hide the api key since it is free and has a 1000/day limit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const fetchData = async () =>{
+    const response = await axios.get("http://www.omdbapi.com/",{
+        params: {
+            apikey: '65d9cda5',
+            s: ''
+        }
+    });
+};
 
+fetchData();
