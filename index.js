37db0cf3ce770b3e2cd0fd6879d03fa2c2e5ff59
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-17 10:37:57 -0400

    added statistic functionality

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ const onInput = async event => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener('click',() =>{
+      dropdown.classList.toggle('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -56,3 +62,53 @@ document.addEventListener('click',event =>{
     dropdown.classList.toggle('is-active');
   }
 })
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '65d9cda5',
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetails) => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetails.Poster}"/>
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetails.Title}</h1>
+        <h4>${movieDetails.Genre}</h4>
+        <p>${movieDetails.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+  <p class="title">${movieDetails.BoxOffice}</p>
+  <p class="subtitle">Box Office</p>
+</article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.Metascore}</p>
+    <p class="subtitle">Meta Score</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+  <p class="title">${movieDetails.imdbVotes}</p>
+  <p class="subtitle">IMDB Votes</p>
+  </article>
+  `;
+}
+  
