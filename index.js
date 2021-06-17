fb2e7e38e3bd4c3a39f5947a669f39fc2814f3cc
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-17 10:02:50 -0400

    added click event listener to get rid of the drop down and to update the input text when a user selects a movie

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
+
+  if(!movies.length) {
+    dropdown.classList.toggle('is-active');
+  }
   resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
