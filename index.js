0299f655a8ee2cccbaf38ac7233b296b777dc9ab
Johnathan Graybeal <graybealjohnathan@gmail.com>
2021-06-18 14:02:59 -0400

    refactored to amke rendering the page more reusable

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ const fetchData = async searchTerm => {
 };
 
 createAutoComplete({
-  root: document.querySelector('.autocomplete')
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie){
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+    <img src="${imgSrc}" />
+    ${movie.Title} (${movie.Year})
+  `;
+  }
 });
-createAutoComplete({
-  root: document.querySelector('.autocomplete-two')
-});
-createAutoComplete({
-  root: document.querySelector('.autocomplete-three')
-});
+
 
 const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
